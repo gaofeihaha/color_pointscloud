7fccc15bbcbf150a54eed09d67af2e89516995cd
GaoLongfei <gaofeisbox@163.com>
2025-12-23 17:21:50 +0800

    测试

---
+++ include/color_points_component.hpp
@@ -0,0 +1,159 @@
+#ifndef COLOR_POINTS_COMPONENT_HPP
+#define COLOR_POINTS_COMPONENT_HPP
+
+#include <memory>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <unordered_map>
+#include <omp.h>
+#include <chrono>
+
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp_components/register_node_macro.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#include <sensor_msgs/msg/imu.hpp>
+#include "sensor_msgs/msg/nav_sat_fix.hpp"
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/common/transforms.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <image_transport/image_transport.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/eigen.hpp>
+#include <yaml-cpp/yaml.h>
+#include <Eigen/Dense>
+#include <Eigen/Geometry>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/buffer.h>
+#include <tf2_eigen/tf2_eigen.hpp>
+#include <std_srvs/srv/set_bool.hpp>
+
+#include "bag_manager.hpp"
+
+using PointCloud2 = sensor_msgs::msg::PointCloud2;
+using Image = sensor_msgs::msg::CompressedImage;
+using namespace std::chrono_literals;
+
+namespace color_pointscloud
+{
+
+struct PointXYZIRCAEDT
+{
+  PCL_ADD_POINT4D
+  std::uint8_t intensity{0U};
+  std::uint8_t return_type{0U};
+  std::uint16_t channel{0U};
+  float azimuth{0.0F};
+  float elevation{0.0F};
+  float distance{0.0F};
+  std::uint32_t time_stamp{0U};
+}EIGEN_ALIGN16;
+
+struct PointXYZRGBT
+{
+  PCL_ADD_POINT4D;
+  union
+  {
+    struct { uint8_t b,g,r,a; }rgba_struct;
+    uint32_t rgba;
+  };
+  std::uint32_t time_stamp{0U};
+}EIGEN_ALIGN16;
+
+// 数据结构定义
+struct ExtrinsicParams {
+    Eigen::Matrix3f rotation;
+    Eigen::Vector3f translation;
+    Eigen::Matrix4f transformation;
+};
+
+struct IntrinsicParams {
+    double fx;
+    double fy;
+    double cx;
+    double cy;
+    std::vector<double> k;
+    Eigen::Matrix3f camera_matrix;
+};
+
+struct PointCloudConfig {
+    std::string topic;
+    std::string frame_id;
+    ExtrinsicParams extrinsic;
+};
+
+struct ImageConfig {
+    std::string topic;
+    std::string name;
+    IntrinsicParams intrinsic;
+    ExtrinsicParams extrinsic;
+};
+
+class ColorPointsComponent : public rclcpp::Node
+{
+public:
+    explicit ColorPointsComponent(const rclcpp::NodeOptions & options);
+    ~ColorPointsComponent();
+
+private:
+    BagManager *bag_manager_;
+    size_t gnss_count = 0;
+    size_t imu_count = 0;
+    size_t points_count = 0;
+
+    std::vector<std::pair<cv::Mat, cv::Mat>> undistort_maps_;
+    std::vector<cv::Size> image_size_cache_;
+
+    struct GeneralConfig {
+        int image_queue_size;
+        double max_time_diff;
+        bool use_tf;
+        std::string output_topic;
+    };
+
+    GeneralConfig general_config_;
+    std::vector<PointCloudConfig> pointcloud_configs_;
+    std::vector<ImageConfig> image_configs_;
+
+    // ROS2相关
+    rclcpp::Publisher<PointCloud2>::SharedPtr colored_cloud_pub_;
+    std::vector<std::shared_ptr<message_filters::Subscriber<PointCloud2>>> cloud_subs_;
+    std::vector<std::shared_ptr<message_filters::Subscriber<Image>>> image_subs_;
+    rclcpp::Subscription<sensor_msgs::msg::NavSatFix>::SharedPtr gnss_sub_;
+    rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_sub_;
+
+
+    std::shared_ptr<tf2_ros::Buffer> tf_buffer_;
+    std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+    rclcpp::Service<std_srvs::srv::SetBool>::SharedPtr bag_control_service_;
+
+    using SyncPolicy = message_filters::sync_policies::ApproximateTime<
+        PointCloud2, PointCloud2, Image, Image>;
+    std::shared_ptr<message_filters::Synchronizer<SyncPolicy>> sync_;
+
+
+    bool loadConfig(const std::string& config_file);
+    cv::Mat processImage(const Image::ConstSharedPtr& image_msg, size_t index);
+    pcl::PointCloud<PointXYZRGBT>::Ptr colorPointCloud(const pcl::PointCloud<PointXYZRGBT>::Ptr& cloud,const std::vector<cv::Mat>& images);
+    void printConfigSummary();
+    void initSubscribers();
+    void initSynchronizer();
+    void initServices();
+    void syncCallback(const PointCloud2::ConstSharedPtr& cloud1_msg, 
+                  const PointCloud2::ConstSharedPtr& cloud2_msg, 
+                  const Image::ConstSharedPtr& image1_msg, 
+                  const Image::ConstSharedPtr& image2_msg);
+    void handleBagControl(const std::shared_ptr<std_srvs::srv::SetBool::Request> request,
+                       const std::shared_ptr<std_srvs::srv::SetBool::Response> response);
+};
+
+} // namespace color_pointscloud
+
+#endif // COLOR_POINTS_COMPONENT_HPP
---
+++ launch/color_points_component.launch.py
@@ -0,0 +1,64 @@
+# import launch
+# from launch_ros.actions import ComposableNodeContainer
+# from launch_ros.descriptions import ComposableNode
+
+# def generate_launch_description():
+#     """Launch color_pointscloud component in nvenc_multicam_container."""
+    
+#     container = ComposableNodeContainer(
+#         name='nvenc_multicam_container',
+#         namespace='',
+#         package='rclcpp_components',
+#         executable='component_container',
+#         composable_node_descriptions=[
+#             # 添加你的color_pointscloud组件
+#             ComposableNode(
+#                 package='color_pointscloud',
+#                 plugin='color_pointscloud::ColorPointsComponent',
+#                 name='color_points_component',
+#                 parameters=[
+#                     # 从YAML文件加载参数
+#                     {'use_sim_time': False},
+#                     # 可以在这里添加其他参数
+#                 ],
+#                 # 如果需要重映射话题
+#                 remappings=[
+#                     # 例如: ('input_topic', 'new_topic_name')
+#                 ]
+#             ),
+#             # 你还可以在这里添加其他组件
+#         ],
+#         output='screen',
+#         arguments=['--ros-args', '--log-level', 'info'],
+#     )
+
+#     return launch.LaunchDescription([container])
+
+import launch
+from launch.actions import ExecuteProcess
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    # 首先启动容器（如果还没有运行）
+    container = Node(
+        package='rclcpp_components',
+        executable='component_container',
+        name='nvenc_multicam_container',
+        output='screen'
+    )
+    
+    # 然后加载组件
+    load_component = ExecuteProcess(
+        cmd=[
+            'ros2', 'component', 'load',
+            '/nvenc_multicam_container',
+            'color_pointscloud',
+            'color_pointscloud::ColorPointsComponent',
+            '--node-name', 'color_points_component',
+            # 如果需要参数
+            '-p', 'use_sim_time:=false'
+        ],
+        output='screen'
+    )
+    
+    return launch.LaunchDescription([container, load_component])
